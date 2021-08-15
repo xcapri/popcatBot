dd237f57ffc36e98e55991ad3d57f4064806f6bc
xcapri <43540712+xcapri@users.noreply.github.com>
2021-08-15 10:56:19 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,42 +1,14 @@
-#!/usr/bin/env python3
-from Crypto.Util.number import getPrime
-import re
-import sys
+from selenium import webdriver
 
-class Unbuffered(object):
-    def __init__(self, stream):
-        self.stream = stream
-    def write(self, data):
-        self.stream.write(data)
-        self.stream.flush()
-    def writelines(self, datas):
-        self.stream.writelines(datas)
-        self.stream.flush()
-    def __getattr__(self, attr):
-        return getattr(self.stream, attr)
 
-sys.stdout = Unbuffered(sys.stdout)
+def ponCat():
+    pondev = webdriver.Chrome("D:/bot/chromedriver_win32/chromedriver")
+    # set path ke dir spesifik file chromedriver anda 
 
-FLAG = open('flag.txt', 'rb').read()
-
-def generate(nbit):
-    x = nbit // 16
-    e = getPrime(x * 5)
-    p = getPrime(x * 7)
-    q = getPrime(x * 9)
-    return [e, p * q]
-
-def main():
-    e, n = generate(224)
-    m = int.from_bytes(re.findall(br'^gemastik14{(\w+)}$', FLAG)[0], 'big')
-    assert m < n
-    c = pow(m, e, n)
-    print(f'<< {c}')
-    for _ in range(3):
-        m = int(input('>> '))
-        assert m > 0
-        c = pow(m, e, n)
-        print(f'<< {c}')
-
-if __name__ == '__main__':
-    main()
+    getlink = 'https://popcat.click/'
+    
+    pondev.get(getlink)
+    while True:
+        pondev.find_element_by_xpath("//*[@id='app']").click() #auto klik popcat 
+  
+ponCat()
